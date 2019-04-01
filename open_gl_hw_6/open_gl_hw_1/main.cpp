16dcae4033db96939a1b775be026536b067d2b76
Shady Boukhary <sb199898.sb@gmail.com>
2019-03-31 21:03:15 -0500

    program 6 solar system

---
+++ open_gl_hw_6/open_gl_hw_1/main.cpp
@@ -0,0 +1,260 @@
+﻿// Shady Boukhary
+// Intro to Graphics
+// Dr. Simpson
+// April 1sth, 2019
+//
+// Renders a solar system with rotating and orbiting planets
+
+
+#include "Angel.h"
+#include "Model.h"
+#include <time.h>
+#include <stack>
+
+GLuint program;
+enum Bodies {sun, earth, moon, mercury, mars, saturn, tesla};
+// Array of rotation angles (in degrees) for each coordinate axis
+enum { Xaxis = 0, Yaxis = 1, Zaxis = 2, NumAxes = 3 };
+enum {earthTheta, moonTheta, mercuryTheta, numTheta = 3};
+double zoom = 30;
+double xEye = 0;
+int      Axis = Xaxis;
+GLfloat  Theta[NumAxes] = { 0.0, 0.0, 0.0 };
+GLfloat BodyTheta[numTheta] = { 0.0, 0.0, 360 };
+const int NUM_MODELS = 7;
+Model models[NUM_MODELS] = {	Model("./models/sun.ply"), Model("./models/earth.ply"), Model("./models/earthmoon.ply"),
+								Model("./models/mercury.ply"), Model("./models/mars.ply"), Model("./models/saturn.ply"),
+								Model("./models/tesla.ply") 
+							};
+int currentModel = 0;
+GLint modelViewLoc;
+GLint projectionLoc;
+
+// initialize OpenGL
+void init();
+
+// OpenGL display callback
+void display();
+
+// ModelView rotaion matrix
+mat4 rotationMatrix(double y, double z);
+
+// OpenGL reshape callback
+void reshape(int, int);
+
+// OpenGL keyboard callback
+void keyboard(unsigned char, int, int);
+
+// OpenGL mouse callback
+void mouse(int, int, int, int);
+
+// OpenGL idle callback
+void idle();
+
+
+// OpenGL initialization
+void init()
+{
+	// Load shaders and use the resulting shader program
+	program = InitShader("vshader_a6.glsl", "fshader_a6.glsl");
+	glUseProgram(program);
+
+	for (int x = 0; x < NUM_MODELS; x++) {
+		models[x].Load(program);
+	}
+	// get modelView and projection locations
+	modelViewLoc = glGetUniformLocation(program, "ModelView");
+	projectionLoc = glGetUniformLocation(program, "Projection");
+
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+// Display Callback
+void display()
+{
+	glClearColor(0, 0, 0, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	stack<mat4> mvs;
+	
+
+	// look at origin
+	vec4 at(0.0, 0.0, 0.0, 1.0);
+	// camera position 2 units in the positive z
+	vec4 eye(0, 0, zoom, 1.0);
+	// up vector
+	vec4 up(0.0, 1.0, 0.0, 0.0);
+
+	// modalView matrix for the sun
+	mat4 modelView = LookAt(eye, at, up);
+	mvs.push(modelView);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView * rotationMatrix(10, 40));
+	models[Bodies::sun].Draw(program);
+	// update modalView uniform variable
+
+	// earth
+	modelView = modelView * RotateZ(BodyTheta[earthTheta]) * Translate(5, 0, 0)* Scale(.3, .3, .3);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView  * rotationMatrix(33, 0));
+	models[Bodies::earth].Draw(program);
+
+	// moon 
+	modelView = modelView * RotateZ(BodyTheta[moonTheta]) * Translate(3, 0, 0)* Scale(.3, .3, .3);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView);
+	models[Bodies::moon].Draw(program);
+
+	// mercury
+	modelView = mvs.top();
+	modelView = modelView * RotateZ(BodyTheta[mercuryTheta]) * Translate(3, 0, 0)* Scale(.2, .2, .2);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView);
+	models[Bodies::mercury].Draw(program);
+
+	// mars
+	modelView = mvs.top();
+	modelView = modelView * RotateZ(BodyTheta[mercuryTheta]) * Translate(7, 0, 0)* Scale(.4, .4, .4);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView);
+	models[Bodies::mars].Draw(program);
+
+	// saturn
+	modelView = mvs.top();
+	modelView = modelView * RotateZ(BodyTheta[moonTheta]) * Translate(10, 0, 0)* Scale(.5, .5, .5);
+	mvs.push(modelView);
+	double saturnTheta = BodyTheta[moonTheta];
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView * rotationMatrix(saturnTheta, saturnTheta));
+	models[Bodies::saturn].Draw(program);
+
+	// saturn moon1
+	modelView = modelView * RotateZ(BodyTheta[moonTheta]) * Translate(3, 0, 0)* Scale(.3, .3, .3);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView * rotationMatrix(saturnTheta, saturnTheta));
+	models[Bodies::moon].Draw(program);
+
+	// saturn moon2
+	modelView = mvs.top();
+	mvs.pop();
+	modelView = modelView * RotateZ(BodyTheta[mercuryTheta]) * Translate(2, 0, 0)* Scale(.2, .2, .2);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView * rotationMatrix(saturnTheta, saturnTheta));
+	models[Bodies::moon].Draw(program);
+
+	// tesla
+	modelView = mvs.top();
+	modelView = modelView * RotateZ(BodyTheta[mercuryTheta]) * Translate(6, 0, 0)* Scale(.05, .05, .05);
+	glUniformMatrix4fv(modelViewLoc, 1, GL_TRUE, modelView * rotationMatrix(saturnTheta, saturnTheta));
+	models[Bodies::tesla].Draw(program);
+
+	// Calculate frame rate
+	static int frame, fps, time, timebase = 0;
+	frame++;
+	time = glutGet(GLUT_ELAPSED_TIME);
+	char display_string[100];
+	if (time - timebase > 1000)
+	{
+		fps = frame * 1000.0 / (time - timebase);
+		sprintf(display_string, "Shady's rotating models : FPS:%d ", fps);
+		glutSetWindowTitle(display_string);
+		timebase = time;
+		frame = 0;
+	}
+	glutSwapBuffers();
+}
+
+mat4 rotationMatrix(double y, double z)
+{
+	return RotateX(Theta[Xaxis]) * RotateY(y) * RotateZ(z);
+}
+
+void reshape(int width, int height)
+{
+	glViewport(0, 0, width, height);
+
+	GLfloat aspect = GLfloat(width) / height;
+	mat4  projection = Perspective(45.0, aspect, 0.1, 150);
+
+	glUniformMatrix4fv(projectionLoc, 1, GL_TRUE, projection);
+}
+
+// Keyboard Callback
+void keyboard(unsigned char key, int x, int y)
+{
+	switch (key) {
+	case 033: // Escape Key
+	case 'q': case 'Q':
+		exit(EXIT_SUCCESS);
+		break;
+	case 'w':
+		xEye++;
+		break;
+	case 's':
+		xEye--;
+		break;
+	}
+}
+
+// Mouse Callback
+void mouse(int button, int state, int x, int y)
+{
+	if (state == GLUT_DOWN) {
+		switch (button) {
+		case GLUT_LEFT_BUTTON:    Axis = Xaxis;  break;
+		case GLUT_MIDDLE_BUTTON:  Axis = Yaxis;  break;
+		case GLUT_RIGHT_BUTTON:   Axis = Zaxis;  break;
+		case 3:					  zoom++;		 break;
+		case 4:					  zoom--;		 break;
+		}
+	} 
+
+}
+
+// Idle Callback
+void idle()
+{
+	Theta[Axis] += 0.01;
+
+	if (Theta[Axis] > 360.0) {
+		Theta[Axis] -= 360.0;
+	}
+
+	BodyTheta[earthTheta] += 0.01;
+
+	if (BodyTheta[earthTheta] > 360.0) {
+		BodyTheta[earthTheta] -= 360.0;
+	}
+
+	BodyTheta[moonTheta] += 0.01;
+
+	if (BodyTheta[moonTheta] > 360.0) {
+		BodyTheta[moonTheta] -= 360.0;
+	}
+
+	BodyTheta[mercuryTheta] -= 0.01;
+
+	if (BodyTheta[mercuryTheta] <= 360.0) {
+		BodyTheta[mercuryTheta] += 360.0;
+	}
+
+	glutPostRedisplay();
+}
+
+
+int main(int argc, char **argv)
+{
+	glewExperimental = GL_TRUE;
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+	
+	glutInitWindowSize(512, 512);
+	glutInitContextVersion(3, 2);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutCreateWindow("Color Cube");
+
+	glewInit();
+
+	init();
+
+	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+	glutMouseFunc(mouse);
+	glutReshapeFunc(reshape);
+	glutIdleFunc(idle);
+
+	glutMainLoop();
+	return 0;
+}
---
+++ open_gl_hw_6/open_gl_hw_1/plyloader.cpp
@@ -0,0 +1,223 @@
+//  The following software has been modified by R. P. Simpson to work with ASCII
+//  blender files.  It originally read binary file.  The original's use of glm::
+// was converted to use Angel:: on all the vec3,vec4 classes.  A new struct called ivec3
+// was added to the code since Angel did not contain support for the integer vec.
+// This software is a very simple loader for Blender file.  It does not have the 
+// general flexibility of other loaders that you may download such as YaPly loader.
+// It would be worth your while to make a study of the other loaders in order to 
+// learn some c++11 tricks of the trade, if so inclined.
+// I also ripped out code that delta with cases that did not include
+// color and normals.
+
+/**************************************
+* Copyright (c) <2014> <Vishesh Gupta> *
+**************************************
+
+The MIT / X Window System License
+=================================
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include "plyloader.h"
+#pragma comment( lib, "glew32" )
+
+using namespace std;
+using namespace Angel;
+
+PLYModel::PLYModel() {
+}
+
+struct data
+{
+	float _x, _y, _z;
+    float _nx, _ny, _nz;
+    int _r, _g, _b, _a;
+	data(){
+		 _x= _y=_z=_nx=_ny=_nz=_r=_g=_b=_a=0;
+	}
+};
+
+
+struct faceData
+{
+	int a,b,c;
+	faceData()
+	{
+		a=b=c=0;
+	}
+};
+
+struct colorData
+{
+	int r,g,b,a;
+	colorData(){
+		r=g=b=a=0;
+	}
+};
+
+// This version will only work with ply files that have
+// BOTH normals and colors.  Modified By Richard P. Simpson
+PLYModel::PLYModel(const char* filename) {
+    
+	long nvertices;
+	int position;
+	struct faceData F;
+	isMesh=0;
+
+    string line;
+    string s1, s2;
+
+    ifstream inputPly;
+	//inputPly.open(filename,ios::binary);
+	inputPly.open(filename);
+	if (!inputPly.is_open()) {
+        cerr << "Couldn't open " << filename << '\n';
+        exit(1);
+    }
+	cout << "Opened " << filename << endl;
+	ifColor=0;
+	ifNormal=0;
+
+    getline(inputPly, line);
+	while(line.compare("vertex")!=0)
+	{
+		getline(inputPly, line,' ');
+		//cout<<line<<"\n";
+	}
+
+	inputPly>>vertexCount; // 3rd line
+	cout << "Number of verts:" << vertexCount << endl;;
+	
+	//getline(inputPly,line);
+	//bool visit=0;
+	while(line.compare("face")!=0)
+	{
+		if(line.find("red")==0) ifColor=1;
+		if(line.find("nx")==0) ifNormal=1;
+		getline(inputPly, line,' ');
+		//cout<<line<<endl;		
+	}
+
+	inputPly>>faceCount;
+	cout << "Number of faces:" << faceCount << endl;
+	if(faceCount>0)
+		isMesh=1;
+
+	while(line.compare("end_header")!=0)
+	{
+		getline(inputPly, line);
+	}
+	
+	// We will assume that our objects have both COLOR and NORMAL information
+
+	{
+		struct data Values;
+
+		if(!ifColor)
+		{
+			cout<<"\nFile doesn't contain the Color Information, check the constructor call";
+			exit(0);
+		}
+		if(!ifNormal)
+		{
+			cout<<"\nFile doesn't contain the Normal Information, check the constructor call";
+			exit(0);
+		}
+
+		//inputPly.read((char *)&Values,sizeof(Values));
+		inputPly >> Values._x >> Values._y >> Values._z;
+		inputPly >> Values._nx >> Values._ny >> Values._nz;
+		inputPly >> Values._r >> Values._g >> Values._b;
+		//cout<<"\n"<<Values._x <<"\t"<< Values._y <<"\t"<< Values._z <<"\t"<< Values._nx <<"\t"<< Values._ny <<"\t"<< Values._nz <<"\t"<< Values._r <<"\t"<< Values._g <<"\t"<< Values._b <<"\t"<< Values._a;
+		min = max = vec3(Values._x, Values._y, Values._z);
+
+		positions.push_back(vec3(Values._x, Values._y, Values._z));
+		normals.push_back(vec3(Values._nx, Values._ny, Values._nz));
+		colors.push_back(vec3(Values._r, Values._g, Values._b) / 255.0f);
+
+		for (long int i = 1; i < vertexCount; i++) 
+		{
+			//inputPly.read((char *)&Values,sizeof(Values));
+			inputPly >> Values._x >> Values._y >> Values._z;
+			inputPly >> Values._nx >> Values._ny >> Values._nz;
+			inputPly >> Values._r >> Values._g >> Values._b;
+			//cout<<"\n"<<Values._x <<"\t"<< Values._y <<"\t"<< Values._z <<"\t"<< Values._nx <<"\t"<< Values._ny <<"\t"<< Values._nz <<"\t"<< (int)Values._r <<"\t"<< (int)Values._g <<"\t"<< (int)Values._b <<"\t"<< (int)Values._a;
+        
+			if (Values._x < min.x) min.x = Values._x;
+			if (Values._y < min.y) min.y = Values._y;
+			if (Values._z < min.z) min.z = Values._z;
+
+			if (Values._x > max.x) max.x = Values._x;
+			if (Values._y > max.y) max.y = Values._y;
+			if (Values._z > max.z) max.z = Values._z;
+
+			positions.push_back(vec3(Values._x, Values._y, Values._z));	// -1 is been multiplied to set the correct orientation of this model....
+			normals.push_back(vec3(Values._nx, Values._ny, Values._nz));
+			colors.push_back(vec3((int)Values._r, (int)Values._g, (int)Values._b) / 255.0f);
+		}
+		center = (min + max) / 2.0f;
+
+		//Bounding volume measurements
+		bvWidth = max.x - min.x;
+		bvHeight = max.y - min.y;
+		bvDepth = max.z - min.z;
+		bvAspectRatio = bvWidth / bvHeight;
+	}
+
+	
+	if(isMesh)
+	{
+		unsigned int numEdges;
+		for(int i=0;i<faceCount;i++)
+		{
+			//inputPly.read((char *)&numEdges,sizeof(numEdges));
+			inputPly >> numEdges;
+			if (numEdges != 3) {//ADDED: RP Simpson
+				cerr << "Model is NOT triangulated!" << endl;
+				cerr << "Triangulate model in Blender and resave" << endl;
+				exit(1);
+			}
+			//inputPly.read((char *)&F,sizeof(F));
+			inputPly >> F.a >> F.b >> F.c;
+			indices.push_back(F.a);
+			indices.push_back(F.b);
+			indices.push_back(F.c);
+			faces.push_back(ivec3(F.a,F.b,F.c));// I really dont need this. Remove later
+		}
+	}
+    inputPly.close();
+}
+
+
+void PLYModel:: FreeMemory()
+{
+	positions.~vector();
+	normals.~vector();
+	colors.~vector();
+	if(isMesh)
+	{
+		faces.~vector();
+	}
+
+}
